05c88faad7463b35b1662c6cc2a82395fd87e631
freilinjb <freilinjb@gmail.com>
2020-02-18 21:57:11 -0400

    iniciando

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    cout << "Hola mundo" << endl;
+    return 0;
+}

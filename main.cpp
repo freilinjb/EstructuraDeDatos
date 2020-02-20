683358bb09a72c3aac2df8ea5a0235cf5355613b
freilinjb <freilinjb@gmail.com>
2020-02-20 18:33:11 -0400

    modificando el espacio de trabajo

--- main.cpp
+++ main.cpp
@@ -8,5 +8,10 @@ int main(int argc, char const *argv[])
 {
     
     cout << "Hola mundo" << endl;
+    string nombre;
+
+    getline(cin, nombre);
+
+    cout << "Su nombre es: " << nombre << endl;
     return 0;
 }

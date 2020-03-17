9daaf02554c2dc349019e581cc503268311744e0
freilinjb <freilinjb@gmail.com>
2020-03-17 13:49:09 -0400

    integrando proceso cola

--- colaEjercicioII.cpp
+++ colaEjercicioII.cpp
@@ -10,15 +10,14 @@ using namespace std;
 
 class Pila2
 {
-protected:
+
+public:
+    // void procesar();
     struct Nodo
     {
         string dato;
         Nodo *siguiente;
     } typedef *pila;
-
-public:
-    // void procesar();
 
     void agregarPila(pila &p, string n)
     {
@@ -31,11 +30,17 @@ public:
         cout << "Elemento agregado a la pila: " << n << endl;
     }
 
+    static void limpiarBuffer2()
+    {
+        fflush(stdin);
+        cin.clear(); // unset failbit
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
     void limpiarBuffer()
     {
         fflush(stdin);
         cin.clear(); // unset failbit
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     string sacarPila(pila &p)
@@ -98,7 +103,6 @@ public:
         int op;
         string x; // numero que devuelve la funcon pop
 
-
         do
         {
             menu();
@@ -112,13 +116,13 @@ public:
                 limpiarBuffer();
 
                 getline(cin, dato);
-                
+
                 agregarPila(p, dato);
                 cout << "\n\n\t\tEstudiante " << dato << " apilado...\n\n";
                 break;
 
             case 2:
-                
+
                 x = sacarPila(p);
                 cout << "\n\n\t\tEstudiante " << x << " desapilado...\n\n";
                 limpiarBuffer();
@@ -126,7 +130,7 @@ public:
                 break;
 
             case 3:
-                limpiarBuffer(); 
+                limpiarBuffer();
 
                 cout << "\n\n MOSTRANDO PILA\n\n";
                 if (p != NULL)
@@ -160,10 +164,104 @@ int main(int argc, char const *argv[])
     Pila2 pila;
     pila.procesar();
     return 0;
-
 }
 
-/*
+class Cola : protected Pila2
+{
+public:
+    Pila2 Pila;
+    struct cola
+    {
+        string materia;
+        Pila2::Nodo estudiante;
+        Pila2::Nodo *delante;
+        Pila2::Nodo *atras;
+
+        cola(const string materia)
+        {
+            this->materia = materia;
+        }
+
+        cola() {}
+
+        void encolar(cola &q, const string dato)
+        {
+            Pila2::Nodo *aux = new Pila2::Nodo();
+
+            aux->dato = dato;
+            aux->siguiente = NULL;
+
+            q.delante == NULL ? q.delante = aux : (q.atras)->siguiente = aux;
+
+            q.atras = aux;
+            q.materia = aux->dato;
+        }
+
+        string desencolar(cola &q)
+        {
+            string dato;
+            Nodo *aux;
+
+            aux = q.delante;
+            dato = aux->dato;
+            q.delante = (q.delante)->siguiente;
+            delete (aux);
+
+            return dato;
+        }
+
+        void mostrarCola(cola q)
+        {
+            Nodo *aux;
+            aux = q.delante;
+
+            while (aux != NULL)
+            {
+                cout << "[" << aux->dato << "]" << endl;
+                aux = aux->siguiente;
+            }
+
+            cout << "\n\n**[precione una tecla para continuear]**" << endl;
+            limpiarBuffer2();
+            getchar();
+        }
+
+        void vaciarCola(cola &q)
+        {
+            Pila2::Nodo *aux;
+
+            while (q.delante != NULL)
+            {
+                aux = q.delante;
+                q.delante = aux->siguiente;
+                delete (aux);
+            }
+
+            q.delante = NULL;
+            q.atras = NULL;
+        }
+
+        void menu()
+        {
+            cout << "\n\t IMPLEMENTACION DE COLAS EN C++\n\n";
+            cout << " 1. ENCOLAR                               " << endl;
+            cout << " 2. DESENCOLAR                            " << endl;
+            cout << " 3. MOSTRAR COLA                          " << endl;
+            cout << " 4. VACIAR COLA                           " << endl;
+            cout << " 5. SALIR                                 " << endl;
+
+            cout << "\n INGRESE OPCION: ";
+        }
+
+        void procesar() {
+            struct cola q;
+
+            q.delante = NULL;
+            q.atras = NULL;
+        }
+    };
+};
+
 struct Nodo
 {
     string dato;
@@ -242,7 +340,7 @@ void mostrarCuentas()
 
     int cantidad = 0;
     int width = 25;
-    // cout << left << "Num" << setw(width) << "fecha" << setw(width) << "debito" <<setw(width) << "credito" << endl;
+    cout << left << "Num" << setw(width) << "fecha" << setw(width) << "debito" << setw(width) << "credito" << endl;
     cout << right << "MOVIMIENTO" << setw(width) << "CUENTA" << setw(width) << "DEBITO" << setw(width) << "CREDITO" << setw(width) << "HORA"
          << ""
          << "|" << setw(width) << "FECHA" << endl;
@@ -310,4 +408,3 @@ int main(int argc, char const *argv[])
     cout << "prueba de texto" << endl;
     return 0;
 }
-*/
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <iostream>
 #include <iomanip>
+#include <limits>
+
 #include <list>
 
 using namespace std;
 
 class Pila2
 {
-protected:
+
+public:
+    // void procesar();
     struct Nodo
     {
         string dato;
         Nodo *siguiente;
     } typedef *pila;
-
-public:
-    // void procesar();
 
     void agregarPila(pila &p, string n)
     {
@@ -29,6 +30,19 @@ public:
         cout << "Elemento agregado a la pila: " << n << endl;
     }
 
+    static void limpiarBuffer2()
+    {
+        fflush(stdin);
+        cin.clear(); // unset failbit
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    }
+    void limpiarBuffer()
+    {
+        fflush(stdin);
+        cin.clear(); // unset failbit
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    }
+
     string sacarPila(pila &p)
     {
         string dato;
@@ -39,6 +53,8 @@ public:
         p = aux->siguiente;
 
         delete (aux);
+
+        limpiarBuffer();
 
         return dato;
     }
@@ -65,11 +81,12 @@ public:
             cout << "[" << aux->dato << "]" << endl;
             aux = aux->siguiente;
         }
+        limpiarBuffer();
     }
 
     void menu()
     {
-        cout << "\n\t IMPLEMENTACION DE PILAS EN C++\n\n";
+        cout << "\n\t IMPLEMENTACION DE ESTUDIANTES EN PILAS EN C++\n\n";
         cout << " 1. APILAR                                " << endl;
         cout << " 2. DESAPILAR                             " << endl;
         cout << " 3. MOSTRAR PILA                          " << endl;
@@ -97,28 +114,37 @@ public:
             case 1:
 
                 cout << "\n Estudiante: ";
+                limpiarBuffer();
+
                 getline(cin, dato);
                 
                 agregarPila(p, dato);
-                cout << "\n\n\t\tNumero " << dato << " apilado...\n\n";
+                cout << "\n\n\t\tEstudiante " << dato << " apilado...\n\n";
                 break;
 
             case 2:
-
+                
                 x = sacarPila(p);
-                cout << "\n\n\t\tNumero " << x << " desapilado...\n\n";
+                cout << "\n\n\t\tEstudiante " << x << " desapilado...\n\n";
+                limpiarBuffer();
+
                 break;
 
             case 3:
+                limpiarBuffer(); 
 
                 cout << "\n\n MOSTRANDO PILA\n\n";
                 if (p != NULL)
                     mostrarPila(p);
                 else
+                {
                     cout << "\n\n\tPila vacia..!" << endl;
+                    limpiarBuffer();
+                }
                 break;
 
             case 4:
+                limpiarBuffer();
 
                 destruir_pila(p);
                 cout << "\n\n\t\tPila eliminada...\n\n";
@@ -127,8 +153,6 @@ public:
 
             cout << endl
                  << endl;
-            system("pause");
-            system("cls");
 
         } while (op != 5);
 
@@ -141,4 +165,76 @@ int main(int argc, char const *argv[])
     Pila2 pila;
     pila.procesar();
     return 0;
+
 }
+
+
+class Cola : protected Pila2 { 
+    public:
+    Pila2 Pila;
+    struct cola {
+        string materia;
+        Pila2::Nodo estudiante;
+        Pila2::Nodo *delante;
+        Pila2::Nodo *atras;
+
+
+        cola(const string materia) {
+            this->materia = materia;
+        }
+
+        cola(){}
+
+        void encolar(cola &q,const string dato){
+            Pila2::Nodo *aux = new Pila2::Nodo();
+
+            aux->dato = dato;
+            aux->siguiente = NULL;
+
+            q.delante == NULL ? q.delante = aux : (q.atras)->siguiente = aux;
+
+            q.atras = aux;
+            q.materia = aux->dato;
+        }
+
+        string desencolar(cola &q) {
+            string dato;
+            Nodo *aux;
+
+            aux = q.delante;
+            dato = aux->dato;
+            q.delante = (q.delante)->siguiente;
+            delete(aux);
+
+            return dato;
+        }
+
+        void mostrarCola(cola q) {
+            Nodo *aux;
+            aux = q.delante;
+
+            while (aux != NULL)
+            {
+                cout << "[" << aux->dato << "]" <<endl;
+                aux = aux->siguiente;
+            }
+
+            cout << "\n\n**[precione una tecla para continuear]**" << endl;
+            limpiarBuffer2();
+            getchar();
+        }
+
+        void vaciarCola(cola &q){
+            Pila2::Nodo *aux;
+            aux = q.delante;
+
+            while (aux != NULL)
+            {
+                cout << "[" <<aux->dato << "]" <<endl;
+                aux = aux->siguiente;
+            }
+            cout << "\n\n**[precione una tecla para continuar]**" <<endl;
+            getchar();
+        }
+    };
+};

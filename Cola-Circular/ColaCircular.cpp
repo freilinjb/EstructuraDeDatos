808b3adc17a34cef78fe2e91d37e061b48fbe48a
freilinjb <freilinjb@gmail.com>
2020-03-20 08:27:44 -0400

    agregndo ejemplos

--- Cola-Circular/ColaCircular.cpp
+++ Cola-Circular/ColaCircular.cpp
@@ -10,4 +10,27 @@ using namespace std;
 struct nodo {
     int dato;
     nodo* siguiente;
-}*primaro, *ultimo;
+}*primero, *ultimo;
+
+void insertarNodo();
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
+//primero = null ultimo = 45   nuevo = 45     45,67,89,12
+//lista circular simple                  45-> primero
+
+void insertarNodo(){
+    nodo* nuevo = new nodo();
+    cout << "Ingrese el dato: ";
+    cin >> nuevo->dato;
+    
+    if(primero == NULL) {
+        primero = nuevo;
+        primero->siguiente = primero;
+    }
+}
